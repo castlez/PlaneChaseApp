00357065e287e2b95cd70fe990173553f9dbd0f0
castlez <castlez93@gmail.com>
2014-12-26 23:41:58 -0800

    update to current
    updated python code and planes text file to current working build

--- Planechase.py
+++ Planechase.py
@@ -2,9 +2,7 @@ import random
 import colorama
 import os
 from colorama import Fore, Back, Style
-import textwrap
 colorama.init()
-
 #Custom planechase game!
 
 
@@ -22,17 +20,21 @@ def print_one(plane):
         if (i==0):
             print(Fore.BLACK + Back.WHITE + plane[i] + Style.RESET_ALL)
         if (i==1):
-            print (Fore.BLUE + Back.WHITE + plane[i] + Style.RESET_ALL)
+            print(Fore.BLUE + Back.WHITE + plane[i] + Style.RESET_ALL)
         if (i==2):
             print(Fore.RED + Back.WHITE + plane[i] + Style.RESET_ALL)
         
+        
 
 #function to play a plane
-def play(planes, used):
+def play(planes, used, bool):
     if not planes: #if there are no planes
         print(Style.BRIGHT + Fore.RED + Back.WHITE + "YOU HAVE BEEN PLAYING FAR TOO LONG" + Style.RESET_ALL)
         return
     x = random.randint(0, len(planes)-1)
+    while(planes[x][0].find("PSYCHO")==0 and bool==False):
+        x = random.randint(0, len(planes)-1)
+        continue
     print_one(planes[x])
     used.append(planes[x])
     here=planes[x]
@@ -68,31 +70,85 @@ def main():
 
     here=[]
     
-    
+    planardie = False
     while(True):
-        roll = input("What do? (r to roll, p, planechase, q to quit) ")
-        if(roll == 'r'):  #TODO accept "enter"
-            x = random.randint(1,6)
-            print(x)
-            if(x==1):
-                os.system('cls')
-                print(Style.BRIGHT + Fore.RED + Back.GREEN + "\nCHAOS\n" + Style.RESET_ALL)
-                print_one(here)
-            elif(x==2 or x==3 or x==4 or x==5):
-                os.system('cls')
-                print(Fore.CYAN + Back.RED + "\nBLANK\n" + Style.RESET_ALL)
-                print_one(here)
-            elif(x==6):
-                os.system('cls')
-                print(Fore.WHITE + Back.BLUE + "\nPLANECHASE\n" + Style.RESET_ALL)
-                here=play(planes, used)
-            continue
+        if (planardie):
+            print(Fore.RED + Style.BRIGHT)
+            roll = input("What do? (r to roll, p, planechase, q to quit) ")
+            print(Style.RESET_ALL)
+            if(roll == 'r'):  #TODO accept "enter"
+                x = random.randint(1,6)
+                if (not planardie):
+                    if(x==1):
+                        os.system('cls')
+                        print(Style.BRIGHT + Fore.RED + Back.GREEN + "\nCHAOS\n" + Style.RESET_ALL)
+                        print_one(here)
+                    elif(x==2 or x==3 or x==4 or x==5):
+                        os.system('cls')
+                        print(Fore.CYAN + Back.RED + "\nBLANK\n" + Style.RESET_ALL)
+                        print_one(here)
+                    elif(x==6):
+                        os.system('cls')
+                        print(Fore.WHITE + Back.BLUE + "\nPLANECHASE\n" + Style.RESET_ALL)
+                        here=play(planes, used, planardie)
+                if (planardie):
+                    if(x==1 or x==2):
+                        os.system('cls')
+                        print(Style.BRIGHT + Fore.RED + Back.GREEN + "\nCHAOS\n" + Style.RESET_ALL)
+                        print_one(here)
+                    elif(x==3 or x==4):
+                        os.system('cls')
+                        print(Fore.CYAN + Back.RED + "\nBLANK\n" + Style.RESET_ALL)
+                        print_one(here)
+                    elif(x==5 or x==6):
+                        os.system('cls')
+                        print(Fore.WHITE + Back.BLUE + "\nPLANECHASE\n" + Style.RESET_ALL)
+                        here=play(planes, used, planardie)
+                
+                continue
+        if (not planardie):
+            roll = input("What do? (r to roll, p, planechase, q to quit) ")
+            if(roll == 'r'):  #TODO accept "enter"
+                x = random.randint(1,6)
+                print(x)
+                if (not planardie):
+                    if(x==1):
+                        os.system('cls')
+                        print(Style.BRIGHT + Fore.RED + Back.GREEN + "\nCHAOS\n" + Style.RESET_ALL)
+                        print_one(here)
+                    elif(x==2 or x==3 or x==4 or x==5):
+                        os.system('cls')
+                        print(Fore.CYAN + Back.RED + "\nBLANK\n" + Style.RESET_ALL)
+                        print_one(here)
+                    elif(x==6):
+                        os.system('cls')
+                        print(Fore.WHITE + Back.BLUE + "\nPLANECHASE\n" + Style.RESET_ALL)
+                        here=play(planes, used, psycho)
+                if (planardie):
+                    if(x==1 or x==2):
+                        os.system('cls')
+                        print(Style.BRIGHT + Fore.RED + Back.GREEN + "\nCHAOS\n" + Style.RESET_ALL)
+                        print_one(here)
+                    elif(x==3 or x==4):
+                        os.system('cls')
+                        print(Fore.CYAN + Back.RED + "\nBLANK\n" + Style.RESET_ALL)
+                        print_one(here)
+                    elif(x==5 or x==6):
+                        os.system('cls')
+                        print(Fore.WHITE + Back.BLUE + "\nPLANECHASE\n" + Style.RESET_ALL)
+                        here=play(planes, used, psycho)
+                
+                continue
         if(roll == 'p'):
             os.system('cls')
-            here=play(planes, used)
+            here=play(planes, used, planardie)
             continue
         if(roll == 'q'):
             break
+        if(roll == 'psycho'):
+            planardie = True
+            print (Fore.RED + Style.BRIGHT + 'PSYCHO MODE' + Style.RESET_ALL)
+        
         
     print("thanks for playing")
     return
